867b7da3f7c2ba0f8e1f0db8901555a8fc9f34b0
Fabien <fb531@cam.ac.uk>
2023-01-04 19:46:51 +0000

    Made instructionTag private and added getter for it

--- src/main/java/fraglet/instructions/Instruction.java
+++ src/main/java/fraglet/instructions/Instruction.java
@@ -1,9 +1,13 @@
 package fraglet.instructions;
 
 public class Instruction {
-    InstructionTag instructionTag;
+    private InstructionTag instructionTag;
     public Instruction(InstructionTag instructionTag) {
         this.instructionTag = instructionTag;
+    }
+
+    public InstructionTag getInstructionTag() {
+        return instructionTag;
     }
 
     // TODO: Should have some structural mechanism so that a DATA instruction just cannot be created here, and must be done through the DataInstruction class.
