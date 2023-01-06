851f4862f6b97f6808a39a375e79b6a731a06be3
Fabien <fb531@cam.ac.uk>
2023-01-06 00:14:03 +0000

    Tightened access modifiers and some helper methods

--- src/main/java/fraglet/Fraglet.java
+++ src/main/java/fraglet/Fraglet.java
@@ -5,12 +5,27 @@ import fraglet.instructions.Instruction;
 import java.util.LinkedList;
 
 public class Fraglet {
-    LinkedList<Instruction> instructionList;
+    private LinkedList<Instruction> instructionList;
 
     public Fraglet(LinkedList<Instruction> instructionList) {
         this.instructionList = instructionList;
     }
 
+    public LinkedList<Instruction> getInstructionList() {
+        return instructionList;
+    }
 
+    public Instruction peekHeadInstruction() {
+        return instructionList.peekFirst();
+    }
 
+    public boolean isEmpty() {
+        return instructionList.isEmpty();
+    }
+
+    public void consumeNOPHeadInstructions() {
+        while (instructionList.peekFirst().getInstructionTag().isNopWhenAtHead()) {
+            instructionList.removeFirst();
+        }
+    }
 }
