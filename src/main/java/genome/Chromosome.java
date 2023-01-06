cad7acaecb92dca590b7a91f4c9430cba7e79a24
Fabien <fb531@cam.ac.uk>
2023-01-06 00:13:22 +0000

    Corrected data structure types

--- src/main/java/genome/Chromosome.java
+++ src/main/java/genome/Chromosome.java
@@ -2,6 +2,7 @@ package genome;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Chromosome {
 
@@ -11,8 +12,8 @@ public class Chromosome {
 
     // some 2 sided fifo queue (time <- key, name, PID, Param)
 
-    ArrayList<Codon> codonList = new ArrayList<>();
-    HashMap<Integer, Integer> promoterIDToIndexMap = new HashMap<>();
+    ArrayList<Codon> codonList = new ArrayList<>(); // TODO: will need to figure these out, they should all probably be private with getter methods, however not sure about constructors etc.
+    HashMap<Integer, HashSet<Integer>> PIDToIndexMap = new HashMap<>(); // TODO: this will need to be a set of positions
     HashMap<Integer, PromoteRepressPair> codonRegulatoryMap = new HashMap<>();
 
 }
