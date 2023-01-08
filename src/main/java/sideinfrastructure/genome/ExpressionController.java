26779d1072c5fa300d0b788d564cd5b175569020
Fabien <fb531@cam.ac.uk>
2023-01-08 18:34:50 +0000

    deleted as was never used

--- src/main/java/sideinfrastructure/genome/ExpressionController.java
+++
@@ -1,7 +0,0 @@
-package sideinfrastructure.genome;
-
-public class ExpressionController {
-    // needs to do the expression count, expression queue, and probably will include logic for choosing when and where to synthesise new fraglets.
-
-
-}
