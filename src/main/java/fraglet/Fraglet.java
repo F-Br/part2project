cf6660e9b30da3f7eb36ba01d7df3a54e030aeb3
Fabien <fb531@cam.ac.uk>
2023-01-08 18:39:20 +0000

    Added some utility methods. They all seem to follow linkedlists which is the underlying datastructure. Potentially an optimisation here

--- src/main/java/fraglet/Fraglet.java
+++ src/main/java/fraglet/Fraglet.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 public class Fraglet {
     private LinkedList<Instruction> instructionList;
 
-    public Fraglet(LinkedList<Instruction> instructionList) {
+    public Fraglet(LinkedList<Instruction> instructionList) { // TODO: overload this to accept a fraglet
         this.instructionList = instructionList;
     }
 
@@ -23,7 +23,11 @@ public class Fraglet {
         return instructionList.isEmpty();
     }
 
-    public void consumeNOPHeadInstructions() {
+    public Instruction pollHeadInstruction() {
+        return instructionList.pollFirst();
+    }
+
+    public void consumeNOPHeadInstructions() { // TODO: this is potentially very bad, as how would it deal with an empty fraglet?????? Probably best to remove.
         while (instructionList.peekFirst().getInstructionTag().isNopWhenAtHead()) {
             instructionList.removeFirst();
         }
@@ -37,5 +41,29 @@ public class Fraglet {
         catch (IndexOutOfBoundsException e) {
             return null;
         }
+    }
+
+    public void addFirst(Instruction instruction) {
+        instructionList.addFirst(instruction);
+    }
+
+    public int size() {
+        return instructionList.size();
+    }
+
+    public Instruction get(int index) {
+        return instructionList.get(index);
+    }
+
+    public void remove(int index) {
+        instructionList.remove(index);
+    }
+
+    public void addAll(LinkedList<Instruction> seq) { // TODO: overload this to accept a fraglet
+        instructionList.addAll(seq);
+    }
+
+    public void addLast(Instruction instruction) {
+        instructionList.addLast(instruction);
     }
 }
