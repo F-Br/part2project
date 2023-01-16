a64e9712cf0a8f9a3297dcbed1ae91a62141faa1
Fabien <fb531@cam.ac.uk>
2023-01-16 15:53:40 +0000

    Added constructor and code to initialise itself

--- src/main/java/sideinfrastructure/genome/Chromosome.java
+++ src/main/java/sideinfrastructure/genome/Chromosome.java
@@ -1,6 +1,7 @@
 package sideinfrastructure.genome;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -14,6 +15,40 @@ public class Chromosome {
 
     ArrayList<Codon> codonList = new ArrayList<>(); // TODO: will need to figure these out, they should all probably be private with getter methods, however not sure about constructors etc.
     HashMap<Integer, HashSet<Integer>> PIDToIndexMap = new HashMap<>(); // TODO: this will need to be a set of positions
-    HashMap<Integer, PromoteRepressPair> codonRegulatoryMap = new HashMap<>();
+    HashMap<Integer, PromoteRepressPair> codonExpressionMap = new HashMap<>();
+    private ArrayList<Integer> sortedPIDList = new ArrayList<>();
+    private int chromPID;
 
+    public Chromosome(int chromPID, ArrayList<Codon> codonList) {
+        this.chromPID = chromPID;
+        this.codonList = codonList;
+        initialisePIDtoIndexMapAndSortedPIDList();
+    }
+
+    private void initialisePIDtoIndexMapAndSortedPIDList() {
+        int index = 0;
+        for (Codon codon : codonList) {
+            CodonType codonType = codon.getCodonType();
+            if ((codonType == CodonType.BLOCKING_PROMOTER) || (codonType == CodonType.CONTINUING_PROMOTER)) {
+                int PID = codon.getPID();
+
+                sortedPIDList.add(PID);
+                HashSet<Integer> indexSet = PIDToIndexMap.get(PID);
+                if (indexSet == null) {
+                    indexSet = new HashSet<>();
+                }
+                indexSet.add(index);
+                PIDToIndexMap.put(PID, indexSet); // TODO: this line might not be necessary (depending on referencing)
+            }
+            index ++;
+        }
+        sortedPIDList.sort(Comparator.naturalOrder());
+    }
+
+    public ArrayList<Integer> getSortedPIDList() {
+        return sortedPIDList;
+    }
+    public int getChromPID() {
+        return chromPID;
+    }
 }
