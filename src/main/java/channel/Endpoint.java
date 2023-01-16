264dcc813b4e68fa76214e526c74d26e6a4df26b
Fabien <fb531@cam.ac.uk>
2023-01-16 15:55:59 +0000

    Removed inputBufferQueue and added the destinationFragletVat field. Added sendFraglet and setDestinationFragletVat code.

--- src/main/java/channel/Endpoint.java
+++ src/main/java/channel/Endpoint.java
@@ -1,27 +1,39 @@
 package channel;
 
+import fraglet.Fraglet;
 import packet.AbstractPacket;
+import sideinfrastructure.FragletVat;
+import sideinfrastructure.SideIdentifier;
 
 import java.util.Queue;
 
 public class Endpoint {
-    private Queue<AbstractPacket> inputBufferQueue;
     private Pipeline outputPipeline;
+    public FragletVat destinationFragletVat;
+    protected SideIdentifier side;
 
-    public Endpoint(Queue inputBufferQueue, Pipeline outputPipeline) {
-        this.inputBufferQueue = inputBufferQueue;
+    public Endpoint(Pipeline outputPipeline) {
         this.outputPipeline = outputPipeline;
+    }
+
+    public void setDestinationFragletVat(FragletVat destinationFragletVat) {
+        this.destinationFragletVat = destinationFragletVat;
     }
 
     public void sendPacket(AbstractPacket outputPacket) { // TODO: will likely need to make a custom packet for fraglet
         outputPipeline.insertPacket(outputPacket); // TODO: need to check if this works, pipeline class seems fine :), but packet objects and sender + receiver classes seem a bit odd
     }
 
+    public void sendFraglet(Fraglet fraglet) {
+        outputPipeline.insertPacket(new AbstractPacket(fraglet, destinationFragletVat));
+    }
+
+    public SideIdentifier getSide() {
+        return side;
+    }
+
     // TODO: add receive packet
 
-    public Queue getInputBufferQueue() {
-        return inputBufferQueue;
-    }
 
     public void stepUpdate() {
         outputPipeline.stepUpdate();
