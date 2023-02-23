607b369bc45bfcd6ba8b5a8b578b90662055e4b8
Fabien <fb531@cam.ac.uk>
2023-02-23 17:36:09 +0000

    Part of the refactor in how the promoter system works, along with the changes Genome.java has undergone. Now includes adding and removing promoters and repressors, and also getting promote scores

--- src/main/java/sideinfrastructure/genome/Chromosome.java
+++ src/main/java/sideinfrastructure/genome/Chromosome.java
@@ -1,9 +1,6 @@
 package sideinfrastructure.genome;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class Chromosome {
 
@@ -15,9 +12,13 @@ public class Chromosome {
 
     ArrayList<Codon> codonList = new ArrayList<>(); // TODO: will need to figure these out, they should all probably be private with getter methods, however not sure about constructors etc.
     HashMap<Integer, HashSet<Integer>> PIDToIndexMap = new HashMap<>(); // TODO: this will need to be a set of positions
-    HashMap<Integer, PromoteRepressPair> codonExpressionMap = new HashMap<>();
-    private ArrayList<Integer> sortedPIDList = new ArrayList<>();
+    HashMap<Integer, PromoteRepressPair> PIDtoPIDExpressionMap = new HashMap<>();
+    private int totalScore = 0;
+    private ArrayList<Integer> sortedPIDList = new ArrayList<>(); // no duplicates in list
     private int chromPID;
+    private int delegatePID;
+
+    private Random rnd = new Random();
 
     public Chromosome(int chromPID, ArrayList<Codon> codonList) {
         this.chromPID = chromPID;
@@ -31,8 +32,19 @@ public class Chromosome {
             CodonType codonType = codon.getCodonType();
             if ((codonType == CodonType.BLOCKING_PROMOTER) || (codonType == CodonType.CONTINUING_PROMOTER)) {
                 int PID = codon.getPID();
+                PromoteRepressPair PIDExpression = PIDtoPIDExpressionMap.get(PID);
 
-                sortedPIDList.add(PID);
+                // initialise promoteRepressPairs for all PIDs in chromosome
+                if (PIDExpression == null) { // new PID
+                    PIDExpression = new PromoteRepressPair();
+                    PIDtoPIDExpressionMap.put(PID, PIDExpression);
+                    sortedPIDList.add(PID);
+                }
+                else { // already seen PID
+                    PIDExpression.addPIDCountWithinChromosome();
+                }
+
+                // initialise index map for all PIDs in chromosome
                 HashSet<Integer> indexSet = PIDToIndexMap.get(PID);
                 if (indexSet == null) {
                     indexSet = new HashSet<>();
@@ -43,6 +55,7 @@ public class Chromosome {
             index ++;
         }
         sortedPIDList.sort(Comparator.naturalOrder());
+        delegatePID = sortedPIDList.get(0);
     }
 
     public ArrayList<Integer> getSortedPIDList() {
@@ -50,5 +63,84 @@ public class Chromosome {
     }
     public int getChromPID() {
         return chromPID;
+    }
+    public int getDelegatePID() {
+        return delegatePID;
+    }
+
+    // new promoter system:
+    public int addPromoter(int validPID) {
+        if (validPID == chromPID) {
+            validPID = delegatePID;
+        }
+        if (!(sortedPIDList.contains(validPID))) { // if PID not in this chromosome:
+            return totalScore;
+        }
+        PromoteRepressPair promoteRepressPair = PIDtoPIDExpressionMap.get(validPID);
+        totalScore -= promoteRepressPair.getSumOfAllPIDInChromosomePromoteScore();
+        promoteRepressPair.addPromoter();
+        totalScore += promoteRepressPair.getSumOfAllPIDInChromosomePromoteScore();
+        return totalScore;
+    }
+
+    public int removePromoter(int validPID) {
+        if (validPID == chromPID) {
+            validPID = delegatePID;
+        }
+        if (!(sortedPIDList.contains(validPID))) { // if PID not in this chromosome:
+            return totalScore;
+        }
+        PromoteRepressPair promoteRepressPair = PIDtoPIDExpressionMap.get(validPID);
+        totalScore -= promoteRepressPair.getSumOfAllPIDInChromosomePromoteScore();
+        promoteRepressPair.removePromoter();
+        totalScore += promoteRepressPair.getSumOfAllPIDInChromosomePromoteScore();
+        return totalScore;
+    }
+
+    public int addRepressor(int validPID) {
+        if (!(sortedPIDList.contains(validPID))) {
+            return totalScore;
+        }
+        PromoteRepressPair promoteRepressPair = PIDtoPIDExpressionMap.get(validPID);
+        totalScore -= promoteRepressPair.getSumOfAllPIDInChromosomePromoteScore();
+        promoteRepressPair.addRepressor(); // TODO: check that you dont need to put this back into map
+        return totalScore;
+    }
+
+    public int removeRepressor(int validPID) {
+        if (!(sortedPIDList.contains(validPID))) {
+            return totalScore;
+        }
+        PromoteRepressPair promoteRepressPair = PIDtoPIDExpressionMap.get(validPID);
+        totalScore += promoteRepressPair.removeRepressor();
+        return totalScore;
+    }
+
+    public int weightedRandomSelectionOfPID() {
+        int cumulativeScoreTarget = rnd.nextInt(totalScore + 1);
+        int currentCumulativeScore = 0;
+
+        for (int PID : sortedPIDList) {
+            int PIDScore = PIDtoPIDExpressionMap.get(PID).getIndividualPromoteScore();
+
+            if (PIDScore == 0) {
+                continue;
+            }
+
+            currentCumulativeScore += PIDScore;
+            if (cumulativeScoreTarget <= currentCumulativeScore) {
+                return PID;
+            }
+        }
+
+        throw new IllegalStateException("No PID found, should have reached a valid PID or returned null");
+    }
+
+
+
+
+
+    public int getTotalScore() {
+        return totalScore;
     }
 }
