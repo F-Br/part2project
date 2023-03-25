344e22865c5f5c4265f2fe36f1d4eae28adfb177
Fabien <fb531@cam.ac.uk>
2023-03-25 21:49:56 +0000

    Implemented custom toString() function to improve printing

--- src/main/java/fraglet/instructions/Instruction.java
+++ src/main/java/fraglet/instructions/Instruction.java
@@ -11,4 +11,10 @@ public class Instruction {
     }
 
     // TODO: Should have some structural mechanism so that a DATA instruction just cannot be created here, and must be done through the DataInstruction class.
+
+
+    @Override
+    public String toString() {
+        return instructionTag.name();
+    }
 }
