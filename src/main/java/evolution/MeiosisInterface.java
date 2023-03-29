fb9b4796cf3793931d3767b2b7f8823ee37ba38b
Fabien <fb531@cam.ac.uk>
2023-03-29 13:32:50 +0100

    Updated the typing of the interface

--- src/main/java/evolution/MeiosisInterface.java
+++ src/main/java/evolution/MeiosisInterface.java
@@ -1,14 +1,16 @@
 package evolution;
 
+import org.apache.commons.math3.util.Pair;
 import sideinfrastructure.genome.Chromosome;
+import sideinfrastructure.genome.Codon;
 import sideinfrastructure.genome.Genome;
 
 import java.util.List;
 
 public interface MeiosisInterface {
-    Genome performAllMutations(Genome genome);
+    List<Codon> performAllMutations(List<Codon> codonList, int chromosomeIndex);
 
-    ChromosomePair performAllCrossovers(ChromosomePair chromosomePair);
+    Pair<List<Codon>, List<Codon>> performAllCrossovers(Pair<List<Codon>, List<Codon>> pairCodonLists, int chromosomeIndex);
 
     List<Chromosome> performMeiosis(Genome genome);
 
