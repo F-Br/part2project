35e2eb9ac55898b7a8b31704ce460edb4f53c5af
Fabien <fb531@cam.ac.uk>
2023-04-10 12:19:04 +0100

    Defining arguments for getFitness method

--- src/main/java/evolution/fitnessfunctions/FitnessFunction.java
+++ src/main/java/evolution/fitnessfunctions/FitnessFunction.java
@@ -1,6 +1,9 @@
 package evolution.fitnessfunctions;
 
+import sideinfrastructure.ChallengeAnswer;
+import sideinfrastructure.ChallengeQuestion;
+
 public abstract class FitnessFunction {
 
-    abstract public float getFitness();
+    abstract public float getFitness(ChallengeQuestion challengeQuestion, ChallengeAnswer challengeAnswer);
 }
